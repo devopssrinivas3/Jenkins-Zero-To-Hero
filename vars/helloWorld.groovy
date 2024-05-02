91fb27a5798fbcbb915505a0df0cfb4e235535fd
devopssrinivas3 <147231280+devopssrinivas3@users.noreply.github.com>
2024-05-02 11:25:52 +0530

    Update helloWorld.groovy

--- vars/helloWorld.groovy
+++ vars/helloWorld.groovy
@@ -1,9 +1,4 @@
 def call() {
   sh 'echo Hi From DevOps Team Srinivas'
 
-  def numbers = [1, 2, 3, 4, 5]
-def reversedNumbers = numbers.reverse()
-
-println "Original numbers: $numbers"
-println "Reversed numbers: $reversedNumbers"
 }
